83cd8ece2ad7b5685738c143fe9052194ea23ebf
Goncalo <goncalo.cordeiro@protonmail.com>
2025-09-27 21:53:29 +0100

    new components

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+ <nav class="navbar-top">
+    <div class="navbar-topleft">
+      <span class="logo">Blooming Minds Forum</span>
+    </div>
+    <div class="navbar-topright">
+    </div>
+  </nav>
+  <nav class="navbar-bottom">
+    <div class="navbar-bottomleft">
+      <a href="#">Home</a>
+    </div>
+    <div class="navbar-bottomright">
+      <a href="#">Login</a>
+      <a href="#">Sign Up</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,26 +1,17 @@
 <template>
-  <nav class="navbar-top">
-    <div class="navbar-topleft">
-      <span class="logo">Blooming Minds Forum</span>
-    </div>
-    <div class="navbar-topright">
-    </div>
-  </nav>
-  <nav class="navbar-bottom">
-    <div class="navbar-bottomleft">
-      <a href="#">Home</a>
-    </div>
-    <div class="navbar-bottomright">
-      <a href="#">Login</a>
-      <a href="#">Sign Up</a>
-    </div>
-  </nav>
+  <Navbar/>
 </template>
 
 <script>
 import "@/css/Home.css";
+import Navbar from "@/components/Navbar.vue";
 export default
 {
   name: 'HomeView',
+  components:
+  {
+    Navbar
+  }
 }
+
 </script>
