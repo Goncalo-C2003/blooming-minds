f45e763ddd1842f25dffeb02543872b95498a776
Goncalo <goncalo.cordeiro@protonmail.com>
2025-10-07 21:08:14 +0100

    frontend updates

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
   <Navbar/>
+  <div class="main-content">
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the main landing page of the application.</p>
+  </div>
 </template>
 
 <script>
